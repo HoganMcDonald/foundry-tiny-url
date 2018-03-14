cee0e8a08e02b4797bca8f03862206002627d132
Hogan McDonald <hpeter.mcdonald@icloud.com>
2018-03-14 00:28:12 -0500

    counter working in promise chain. abandoning counter.

--- server/controllers/url.controller.js
+++ server/controllers/url.controller.js
@@ -4,11 +4,40 @@ const Counter = require('../models/Counter.model');
 module.exports = class Url {
 
   static new(req, res) {
+    const self = this;
     Counter.findOneAndUpdate({key: 'url'}, { $inc: { value: 1 }})
-      .then(counter => {
-        res.send({value: ++counter.value});
+
+      .then(urlCounter => ++urlCounter.value)
+
+      .then((index) => {
+
+        let endpoint = String(index);
+
+        console.log(endpoint);
+
+        while (endpoint.length < 6) {
+          endpoint = '0' + endpoint;
+        }
+
+        console.log(endpoint);
+
+        endpoint = parseInt(endpoint, 36);
+        
+        console.log(endpoint);
+
+
+        res.send({value: index})
       })
 
+
+
   } // new()
+
+  static _genEndpoint(index) {
+    // convert index to String of length 6 with radix 36
+    let endpoint = String(index);
+    console.log(endpoint);
+    return endpoint;
+  } // _genEndpoint()
 
 } // Url
